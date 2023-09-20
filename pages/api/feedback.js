3505b80913f0312db77bdae2c54bd144b0f506ef
JadFu <yfu5@aceproject.space>
2023-09-20 13:34:39 -0500

    catch feedback from online form

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,3 +1,23 @@
+import fs from "fs";
+import path from "path";
+
 export default function handler(req, res) {
-  res.status(200).json({ message: "Works" });
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedback = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedback,
+    };
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: "success!", feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: "Works" });
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,39 @@
+import { useRef } from "react";
+
 export default function Home() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitForm(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   return (
     <div>
       <h1>Home Page</h1>
-      <form>
+      <form onSubmit={submitForm}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="feedback">Your Feedback</label>
-          <textarea id="feedback" rows="5" />
+          <textarea id="feedback" rows="5" ref={feedbackInputRef} />
         </div>
         <button>send feedback</button>
       </form>
